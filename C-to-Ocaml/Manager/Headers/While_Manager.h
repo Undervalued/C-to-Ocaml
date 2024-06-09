741588fb6aaf504d1e83dc6d107415b46887188d
Le Mais <0xundervalued@protonmail.com>
2024-06-09 13:07:50 +0200

    While Manager

---
+++ C-to-Ocaml/Manager/Headers/While_Manager.h
@@ -0,0 +1,16 @@
+//
+//  Manager.h
+//  Deepmle
+//
+//  Created by Ismael on 01/01/1987.
+//
+
+#ifndef Printf_Manager_h
+#define Printf_Manager_h
+
+#include <stdio.h>
+#include <assert.h>
+#include "lexer.h" //importe les fonctions du fichier lexer.c
+#include "traducteur.h"
+void While_Manager (maillon* start);
+#endif /* While_Manager */
