d9da46ef69ae03ddfcc1aa52eb1b2ccff25ec237
XxropttxX <42070219+Rodevpet@users.noreply.github.com>
2024-06-09 16:38:51 +0200

    Add function features

--- C-to-Ocaml/Manager/src/Printf_Manager.c
+++ C-to-Ocaml/Manager/src/Printf_Manager.c
@@ -2,41 +2,41 @@
 
 #include "./../Headers/Printf_Manager.h"
 
-void Printf_Manager(maillon* maillons){
+void Printf_Manager(maillon* debut){
     // On suppose que l'utilisateur a 2 neurones et n'ecrit pas " printf         () "
     // Mais on ne fait pas confiance a l'utilisateur et donc on saute les espaces.
-    maillons = easy_strings_jumper(maillons); // on saute la (
+    debut = easy_strings_jumper(debut); // on saute la (
     char ligne[256] = "Printf.printf ";
     int compteur_parentheses = 0;
-    while ( !(maillons->lexeme == 'P' && strcmp(maillons->argument, ";") == 0))
+    while ( !(debut->lexeme == 'P' && strcmp(debut->argument, ";") == 0))
     {
         printf("%s\n", ligne);
         if(compteur_parentheses != 0){
             
-            if(maillons->lexeme != 'V' && maillons->lexeme != 'S'){
-                strcat(ligne, maillons->argument);
+            if(debut->lexeme != 'V' && debut->lexeme != 'S'){
+                strcat(ligne, debut->argument);
             }
-            if(maillons->lexeme == 'P' && strcmp(maillons->argument, ")") == 0){
+            if(debut->lexeme == 'P' && strcmp(debut->argument, ")") == 0){
                 compteur_parentheses-=1;
                 strcat(ligne, " ");
             }
         }
-        printf("%c\n", maillons->lexeme);
-        printf("%s\n", maillons->argument);
-        if(maillons->lexeme == 'S'){
-            strcat(ligne, maillons->argument);
+        printf("%c\n", debut->lexeme);
+        printf("%s\n", debut->argument);
+        if(debut->lexeme == 'S'){
+            strcat(ligne, debut->argument);
             strcat(ligne, " ");
         }
-        else if(maillons->lexeme == 'V'){
+        else if(debut->lexeme == 'V'){
             strcat(ligne, "!");
-            strcat(ligne, maillons->argument);
+            strcat(ligne, debut->argument);
             strcat(ligne, " ");
         }
-        else if(maillons->lexeme == 'P' && strcmp(maillons->argument, "(") == 0){
-            strcat(ligne, maillons->argument);
+        else if(debut->lexeme == 'P' && strcmp(debut->argument, "(") == 0){
+            strcat(ligne, debut->argument);
             compteur_parentheses += 1;
         }
-        maillons = maillons -> suivant;
+        debut = debut -> suivant;
     }
     printf("%s\n", ligne);
     Line_Writer(ligne);
